5ef351421bf1b43b697ff24d711fa6a57d2ce1cc
nkhandwe <nkhandwe@gmail.com>
2022-01-11 11:40:29 +0530

    Home Design

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,17 @@
+import 'package:flutter/material.dart';
 
+void  main() {
+  runApp(MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(
+        child: Text("Welcome to my app"),
+      )
+    );
+  }
+}
