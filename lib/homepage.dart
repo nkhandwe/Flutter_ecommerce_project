679baab952a2380eab5149b2f59a8c2a69f4219f
nkhandwe <nkhandwe@gmail.com>
2022-01-11 11:57:22 +0530

    app drawer Added

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -1,1 +1,19 @@
 import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+  String welcome = "Welcome To My App";
+    return Scaffold(
+      appBar: AppBar(),
+      body: Center(
+        child: Container(
+        child: Text("$welcome"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,15 @@
+import 'package:ecommerce_app_project/homepage.dart';
 import 'package:flutter/material.dart';
 
-void  main() {
+void main() {
   runApp(MyApp());
 }
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Material(
-        child: Center(
-        child: Container(
-        child: Text("Welcome to my app"),
-        ),
-        ),
-      ),
+      home: HomePage(),
     );
   }
 }
