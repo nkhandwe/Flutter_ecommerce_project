0dfad87e818f1494f75064d53643e1d6afa96ccd
nkhandwe <nkhandwe@gmail.com>
2022-01-11 13:48:32 +0530

    change

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -5,8 +5,14 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      child: Center,
-      child: Text("Splash"),
+      child: Center(
+        child: Text("Splash Page", 
+          style: TextStyle(
+          fontSize: 20,
+          color: Colors.red,
+          fontWeight: FontWeight.bold,
+        ),),
+        ),
     );
   }
 }
