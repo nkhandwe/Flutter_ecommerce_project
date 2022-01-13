f8396f6ae17c868944ab72e9dc0d39c6efe3bc7f
nkhandwe <nkhandwe@gmail.com>
2022-01-13 14:10:21 +0530

    added data to login screen

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -4,22 +4,29 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Material(
-      color: Colors.white,
+        color: Colors.white,
         child: Column(
-      children: [
-        Image.asset(
-          "asset/images/loginpage.png",
-          fit: BoxFit.cover,
-        ),
-        Text(
-          "Welcome",
-          style: TextStyle(
-            fontSize: 22,
-            fontWeight: FontWeight.bold,
-            color: Colors.deepPurple,
-          ),
-        )
-      ],
-    ));
+          children: [
+            Image.asset(
+              "asset/images/loginpage.png",
+              fit: BoxFit.cover,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+           const Text(
+              "Welcome",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+                color: Colors.deepPurple,
+              ),
+            ),
+            const SizedBox(height: 20,),
+            const Padding(padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),),
+            TextFormField(decoration: const InputDecoration(hintText: "Enter Username", labelText: "Username"),),
+            TextFormField(decoration: const InputDecoration(hintText: "Enter Password", labelText: "Password"),),
+          ],
+        ));
   }
 }
