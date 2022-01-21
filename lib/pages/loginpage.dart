991b4aad8bbc123436a89530166d75dfad69781e
nkhandwe <nkhandwe@gmail.com>
2022-01-21 10:21:04 +0530

    added button to login screen

--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -14,7 +14,7 @@ class LoginPage extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-           const Text(
+            const Text(
               "Welcome",
               style: TextStyle(
                 fontSize: 22,
@@ -22,10 +22,31 @@ class LoginPage extends StatelessWidget {
                 color: Colors.deepPurple,
               ),
             ),
-            const SizedBox(height: 20,),
-            const Padding(padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),),
-            TextFormField(decoration: const InputDecoration(hintText: "Enter Username", labelText: "Username"),),
-            TextFormField(decoration: const InputDecoration(hintText: "Enter Password", labelText: "Password"),),
+            const SizedBox(
+              height: 20,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+            ),
+            TextFormField(
+              decoration: const InputDecoration(
+                  hintText: "Enter Username", labelText: "Username"),
+            ),
+            TextFormField(
+              obscureText: true,
+              decoration: const InputDecoration(
+                  hintText: "Enter Password", labelText: "Password"),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            ElevatedButton(
+              child: Text("Login"),
+              style: TextButton.styleFrom(),
+              onPressed: () {
+                print("Hi Nkhandwe");
+              },
+            )
           ],
         ));
   }
